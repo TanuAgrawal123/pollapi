8878addd69b282b3ad550c5941ab1d231cfb0064
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-25 23:44:08 +0530

    serialization data, views and generic views

--- pollsap/polls/urls.py
+++ pollsap/polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from  . import views
 
+from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
 app_name='polls'
 urlpatterns=[
-path("polls/", views.polls_list, name='polls_list'),
-path("polls/<int:pk>/", views.polls_detail, name='polls_detail')]
+path("polls/", PollList.as_view(), name='polls_list'),
+path("polls/<int:pk>/", PollDetail.as_view(), name='polls_detail'),
+path("choices/", ChoiceList.as_view(), name='choice_list'),
+path("vote/", CreateVote.as_view(), name='create_vote'),]
