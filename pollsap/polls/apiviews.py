804952bd3a3791f24276a15326d8491abc051163
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-02-26 15:47:15 +0530

    viewset and routers

--- pollsap/polls/apiviews.py
+++ pollsap/polls/apiviews.py
@@ -1,22 +1,32 @@
 
-from rest_framework import generics
+from rest_framework import generics, status, viewsets
 from .models import Poll, Choice
 from .searializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from rest_framework.views import APIView
 
-
-class PollList(generics.ListCreateAPIView):
-	queryset=Poll.objects.all()
-	serializer_class=PollSerializer
-
-class PollDetail(generics.RetrieveDestroyAPIView):
+class PollViewSet(viewsets.ModelViewSet):
 	queryset=Poll.objects.all()
 	serializer_class=PollSerializer
 
 class ChoiceList(generics.ListCreateAPIView):
-	queryset=Choice.objects.all()
+	def get_queryset(self):
+		queryset=Choice.objects.filter(poll_id=self.kwargs["pk"])
+		return queryset
 	serializer_class=ChoiceSerializer
 
-class CreateVote(generics.CreateAPIView):
-	serializer_class=VoteSerializer
+class CreateVote(APIView):
+	def post(self, request, pk, choice_pk):
+		voted_by=request.data.get("voted_by")
+		data={'choice':choice_pk, 'poll':pk, 'voted_by':voted_by}
+		serializer=VoteSerializer(data=data)
+		if serializer.is_valid():
+			vote=serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		else:
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 	
 
